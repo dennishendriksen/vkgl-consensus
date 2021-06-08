99e5ad7122d72ed04fe61bee020320cb0513ecf8
Dennis Hendriksen <d.hendriksen@umcg.nl>
2021-06-08 13:27:59 +0200

    spring initializr project

---
+++ src/main/java/org/molgenis/vkglconsensus/VkglConsensusApplication.java
@@ -0,0 +1,13 @@
+package org.molgenis.vkglconsensus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkglConsensusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VkglConsensusApplication.class, args);
+	}
+
+}
